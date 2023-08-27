9f84d93220b1a616772dc7d0d894194dcd342b85
Otto Ahoniemi <otto@ottoahoniemi.fi>
2023-08-27 21:04:20 +0300

    Test against regression on queryKeys

---
+++ test/create-endpoint-definitions.test-d.ts
@@ -0,0 +1,24 @@
+import type { QueryKeys } from "@reduxjs/toolkit/dist/query/core/apiState";
+
+import { describe, expectTypeOf, it } from "vitest";
+
+import type { CreateEndpointDefinitions } from "../src/create-endpoint-definitions";
+import type { TRPCBaseQuery } from "../src/create-trpc-base-query";
+import type { AppRouter } from "./fixtures";
+
+describe("create endpoint definitions", () => {
+  describe("for new api (with trpc base query)", () => {
+    // Regression testing against wrong keys returned wrong QueryKeys type
+    it("generates properly shaped return type that can be passed to rtk QueryKeys type helper", () => {
+      type Definitions = CreateEndpointDefinitions<
+        AppRouter,
+        TRPCBaseQuery,
+        "api",
+        never
+      >;
+      expectTypeOf<QueryKeys<Definitions>>().toEqualTypeOf<
+        "getUserById" | "listUsers" | "nested_Deep_GetVeryNestedMessage"
+      >();
+    });
+  });
+});
