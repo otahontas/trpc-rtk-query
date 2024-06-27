f6b038a83671832932878d2163bc646c24b44e58
Otto Ahoniemi <otto@ottoahoniemi.fi>
2024-06-27 10:11:37 +0300

    Add sourcemaps and clean build dir before building again

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
+  clean: true,
+  dts: true,
   format: ["cjs", "esm"],
   minify: true,
+  sourcemap: true,
   treeshake: true,
   tsconfig: "./tsconfig.build.json",
 });
